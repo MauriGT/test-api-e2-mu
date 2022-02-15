f3d2574212c537054cc0757a01a099f2ad08a918
Mauricio Toloza <mauricio.toloza@lmvsoftware.com>
2022-02-14 22:30:23 -0300

    route test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testApi',[TestApiController::class,'index']);
